45d04bca6d70e1d56f001830f68847f3d6a57da0
SSchulze1989 <simonschulze2589@hotmail.de>
2020-08-02 05:33:50 +0200

    Added Templates for IconButton and DropDown and some other default properties in Resource Dictionaries

---
+++ iRLeagueManager/Views/SessionSelectControl.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace iRLeagueManager.Views
+{
+    /// <summary>
+    /// Interaktionslogik für SessionSelectControl.xaml
+    /// </summary>
+    public partial class SessionSelectControl : UserControl
+    {
+        public static readonly DependencyProperty ItemTemplateProperty =
+            DependencyProperty.Register(nameof(ItemTemplate), typeof(DataTemplate), typeof(SessionSelectControl),
+                new PropertyMetadata(null));
+
+        public DataTemplate ItemTemplate
+        {
+            get => (DataTemplate)GetValue(ItemTemplateProperty);
+            set => SetValue(ItemTemplateProperty, value);
+        }
+
+        public SessionSelectControl()
+        {
+            InitializeComponent();
+
+            // Set default Item template
+            //var defaultItemTemplate = TryFindResource("DefaultItemTemplate") as DataTemplate;
+            //if (defaultItemTemplate != null && ItemTemplate == null)
+            //    ItemTemplate = defaultItemTemplate;
+        }
+    }
+}
