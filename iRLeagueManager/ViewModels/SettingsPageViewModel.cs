7c21df3a94ae3e89f522559176a6d3c3e73b037b
Simon Schulze <simonschulze2589@hotmail.de>
2020-09-27 02:52:41 +0200

    Add Incidents settings page

--- iRLeagueManager/ViewModels/SettingsPageViewModel.cs
+++ iRLeagueManager/ViewModels/SettingsPageViewModel.cs
@@ -11,6 +11,8 @@ using iRLeagueManager.Models;
 using iRLeagueManager.Models.Results;
 using iRLeagueManager.Models.Sessions;
 using iRLeagueManager.ViewModels.Collections;
+using iRLeagueManager.Models.Reviews;
+using System.Collections.ObjectModel;
 
 namespace iRLeagueManager.ViewModels
 {
@@ -41,10 +43,23 @@ namespace iRLeagueManager.ViewModels
             }
         }
 
+        private ObservableCollection<VoteCategoryModel> voteCategoriesCollection;
+        private ICollectionView voteCategories;
+        public ICollectionView VoteCategories { get => voteCategories; set => SetValue(ref voteCategories, value); }
+
+        private ObservableCollection<CustomIncidentModel> incidentKindsCollection;
+        private ICollectionView incidentKinds;
+        public ICollectionView IncidentKinds { get => incidentKinds; set => SetValue(ref incidentKinds, value); }
+
         public ICommand AddScoringCmd { get; }
         public ICommand DeleteScoringCmd { get; }
         public ICommand AddScoringTableCmd { get; }
         public ICommand DeleteScoringTableCmd { get; }
+
+        public ICommand AddIncidentKindCmd { get; }
+        public ICommand RemoveIncidentKindCmd { get; }
+        public ICommand AddVoteCategoryCmd { get; }
+        public ICommand RemoveVoteCategoryCmd { get; }
 
         public ICommand SaveChangesCmd { get; }
 
@@ -58,6 +73,10 @@ namespace iRLeagueManager.ViewModels
             DeleteScoringCmd = new RelayCommand(o => DeleteScoring((o as ScoringViewModel).Model), o => o != null);
             DeleteScoringTableCmd = new RelayCommand(o => DeleteScoringTable((o as ScoringTableViewModel).Model), o => o != null);
             SaveChangesCmd = new RelayCommand(async o => await SaveChanges(), o => CanSaveChanges());
+            AddIncidentKindCmd = new RelayCommand(async o => await AddIncidentKind());
+            RemoveIncidentKindCmd = new RelayCommand(async o => await RemoveIncidentKind(o as CustomIncidentModel), o => o != null && o is CustomIncidentModel);
+            AddVoteCategoryCmd = new RelayCommand(async o => await AddVoteCategory());
+            RemoveVoteCategoryCmd = new RelayCommand(async o => await RemoveVoteCategory(o as VoteCategoryModel), o => o != null && o is VoteCategoryModel);
         }
 
         public async Task Load(SeasonModel season)
@@ -78,6 +97,106 @@ namespace iRLeagueManager.ViewModels
                 OnPropertyChanged(null);
 
                 await LeagueContext.GetModelAsync<SeasonModel>(season.ModelId);
+                var incidentKinds = await LeagueContext.GetModelsAsync<CustomIncidentModel>();
+                incidentKindsCollection = new ObservableCollection<CustomIncidentModel>(incidentKinds);
+                SetIncidentKindsView(incidentKindsCollection);
+                var voteCategories = await LeagueContext.GetModelsAsync<VoteCategoryModel>();
+                voteCategoriesCollection = new ObservableCollection<VoteCategoryModel>(voteCategories);
+                SetVoteCategoriesView(voteCategoriesCollection);
+            }
+            catch (Exception e)
+            {
+                GlobalSettings.LogError(e);
+            }
+            finally
+            {
+                IsLoading = false;
+            }
+        }
+
+        private void SetVoteCategoriesView(object source)
+        {
+            VoteCategories = CollectionViewSource.GetDefaultView(source);
+            VoteCategories.SortDescriptions.Add(new SortDescription(nameof(VoteCategoryModel.Index), ListSortDirection.Ascending));
+        }
+
+        private void SetIncidentKindsView(object source)
+        {
+            IncidentKinds = CollectionViewSource.GetDefaultView(source);
+            IncidentKinds.SortDescriptions.Add(new SortDescription(nameof(CustomIncidentModel.Index), ListSortDirection.Ascending));
+        }
+
+        private async Task AddIncidentKind()
+        {
+            try
+            {
+                IsLoading = true;
+                var newIncident = await LeagueContext.AddModelAsync(new CustomIncidentModel());
+                incidentKindsCollection.Add(newIncident);
+            }
+            catch (Exception e)
+            {
+                GlobalSettings.LogError(e);
+            }
+            finally
+            {
+                IsLoading = false;
+            }
+        }
+
+        private async Task RemoveIncidentKind(CustomIncidentModel incidentKind)
+        {
+            if (incidentKind == null)
+            {
+                return;
+            }
+
+            try
+            {
+                IsLoading = true;
+                await LeagueContext.DeleteModelAsync<CustomIncidentModel>(incidentKind.ModelId);
+                incidentKindsCollection.Remove(incidentKind);
+            }
+            catch (Exception e)
+            {
+                GlobalSettings.LogError(e);
+            }
+            finally
+            {
+                IsLoading = false;
+            }
+        }
+
+        private async Task AddVoteCategory()
+        {
+            try
+            {
+                IsLoading = true;
+                var newCategory = await LeagueContext.AddModelAsync(new VoteCategoryModel());
+                voteCategoriesCollection.Add(newCategory);
+            }
+            catch (Exception e)
+            {
+                GlobalSettings.LogError(e);
+            }
+            finally
+            {
+                IsLoading = false;
+            }
+        }
+
+        private async Task RemoveVoteCategory(VoteCategoryModel voteCategory)
+        {
+            if (voteCategory == null)
+            {
+                return;
+            }
+
+            try
+            {
+                IsLoading = true;
+                await LeagueContext.DeleteModelAsync<VoteCategoryModel>(voteCategory.ModelId);
+                voteCategoriesCollection.Remove(voteCategory);
             }
             catch (Exception e)
             {
@@ -97,6 +216,8 @@ namespace iRLeagueManager.ViewModels
                 LeagueContext.ModelManager.ForceExpireModels<SeasonModel>();
                 LeagueContext.ModelManager.ForceExpireModels<ScoringModel>();
                 LeagueContext.ModelManager.ForceExpireModels<ScoringTableModel>();
+                LeagueContext.ModelManager.ForceExpireModels<CustomIncidentModel>();
+                LeagueContext.ModelManager.ForceExpireModels<VoteCategoryModel>();
                 await Load(Season.Model);
                 await base.Refresh();
             }
@@ -211,6 +332,17 @@ namespace iRLeagueManager.ViewModels
                 {
                     await scoringTable.SaveChanges();
                 }
+
+                var updateIncidenKinds = incidentKindsCollection.Where(x => x.ContainsChanges);
+                if (updateIncidenKinds.Count() > 0)
+                {
+                    await LeagueContext.UpdateModelsAsync(updateIncidenKinds);
+                }
+                var updateVoteCategories = voteCategoriesCollection.Where(x => x.ContainsChanges);
+                if (updateVoteCategories.Count() > 0)
+                {
+                    await LeagueContext.UpdateModelsAsync(updateVoteCategories);
+                }
             }
             catch (Exception e)
             {
@@ -238,6 +370,15 @@ namespace iRLeagueManager.ViewModels
             {
                 hasChanges |= scoringTable.Model.ContainsChanges;
             }
+            if (incidentKindsCollection != null)
+            {
+                hasChanges |= incidentKindsCollection.Any(x => x.ContainsChanges);
+            }
+            if (voteCategoriesCollection != null)
+            {
+                hasChanges |= voteCategoriesCollection.Any(x => x.ContainsChanges);
+            }
+
             return hasChanges;
         }
 
