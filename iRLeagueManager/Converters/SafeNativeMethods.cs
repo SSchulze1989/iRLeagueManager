c388fb186a9a7c373cc33387e85c6fbf3dadf570
Simon Schulze <simonschulze2589@hotmail.de>
2020-10-01 11:21:29 +0200

    Add string extension for sorting strings with numbers

---
+++ iRLeagueManager/Converters/SafeNativeMethods.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iRLeagueManager.Converters
+{
+    // Answer from Stackoverflow
+    // https://stackoverflow.com/questions/248603/natural-sort-order-in-c-sharp/248613#248613
+    [SuppressUnmanagedCodeSecurity]
+    internal static class SafeNativeMethods
+    {
+        [DllImport("shlwapi.dll", CharSet = CharSet.Unicode)]
+        public static extern int StrCmpLogicalW(string psz1, string psz2);
+    }
+}
