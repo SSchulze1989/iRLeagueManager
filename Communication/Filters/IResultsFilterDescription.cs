e7ceedf757fd2df8255675d029fad330654f824f
SSchulze1989 <simonschulze2589@hotmail.de>
2020-10-05 12:05:38 +0200

    Add filter description interface

---
+++ Communication/Filters/IResultsFilterDescription.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iRLeagueDatabase.Filters
+{
+    public interface IResultsFilterDescription
+    {
+        string FilterName { get; }
+        string FilterDescription { get; }
+        int NrOfFilterValues { get; }
+        IEnumerable<string> FilterValueDescriptions { get; }
+    }
+}
