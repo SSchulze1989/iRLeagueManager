35e103c526d9af6993bd96b361443e7064e411a8
Simon Schulze <simonschulze2589@hotmail.de>
2020-10-18 00:29:20 +0200

    Adding Filter objects to Communication

---
+++ Communication/Filters/MemberListFilterDescription.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iRLeagueDatabase.Filters
+{
+    public class MemberListFilterDescription : IResultsFilterDescription
+    {
+        public string FilterName => "Member List";
+
+        public string FilterDescription => "Include/Exclude members based on a static list";
+
+        public int NrOfFilterValues => -1;
+
+        public IEnumerable<string> FilterValueDescriptions => new string[] { "Select member" };
+    }
+}
