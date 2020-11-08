fb1addb1bd358c5658fa4a40cfe8865e82a420ce
Simon Schulze <simonschulze2589@hotmail.de>
2020-11-08 19:00:44 +0100

    Add new result edit panel

---
+++ iRLeagueManager/Views/ReviewResultControl.xaml.cs
@@ -0,0 +1,67 @@
+﻿using iRLeagueManager.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace iRLeagueManager.Views
+{
+    /// <summary>
+    /// Interaktionslogik für ReviewResultControl.xaml
+    /// </summary>
+    public partial class ReviewResultControl : UserControl, IModalContent
+    {
+        public IncidentReviewViewModel ViewModel => DataContext as IncidentReviewViewModel;
+
+        public ReviewResultControl()
+        {
+            InitializeComponent();
+        }
+
+        public string Header => "Chose Result";
+
+        public string SubmitText => "Save";
+
+        public string CancelText => "Cancel";
+
+        public bool IsLoading { get; set; }
+
+        public bool CanSubmit()
+        {
+            return true;
+        }
+
+        public void OnCancel()
+        {
+            
+        }
+
+        public void OnLoad()
+        {
+            
+        }
+
+        public async Task<bool> OnSubmitAsync()
+        {
+            try
+            {
+                await ViewModel.SaveChanges();
+            }
+            catch
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
