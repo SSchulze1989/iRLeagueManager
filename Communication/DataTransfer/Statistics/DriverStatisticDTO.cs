2a232be488cdfb1ae956580ff5dc7804fdcedb33
Simon Schulze <simonschulze2589@hotmail.de>
2020-11-18 00:02:12 +0100

    Add statistic data transfer objects

---
+++ Communication/DataTransfer/Statistics/DriverStatisticDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iRLeagueDatabase.DataTransfer.Statistics
+{
+    [DataContract]
+    public class DriverStatisticDTO : MappableDTO
+    {
+        [DataMember]
+        public long StatisticSetId { get; set; }
+        [DataMember]
+        public DriverStatisticRowDTO[] DriverStatisticRows { get; set; }
+
+        public override object[] Keys => new object[] { StatisticSetId };
+    }
+}
