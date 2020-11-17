95587d96dc7008255cc8ca7c79e1fdac5f11482a
Simon Schulze <simonschulze2589@hotmail.de>
2020-11-18 00:03:32 +0100

    Implement weighted average extension method for IEnumerable<double>

---
+++ Communication/Extensions/EnumerableExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iRLeagueDatabase.Extensions
+{
+    public static class EnumerableExtensions
+    {
+        public static void ForEach<T>(this IEnumerable<T> enumerable, Action<T> action)
+        {
+            foreach(var item in enumerable)
+            {
+                action?.Invoke(item);
+            }
+        }
+
+        public static double WeightedAverage<T>(this IEnumerable<T> enumerable, Func<T, double> value, Func<T, double> weight)
+        {
+            // Calculate weighted average by dividing the sum of products between weight and value by the total sum of all weights.
+            var totalWeight = enumerable.Sum(x => weight(x));
+            return (enumerable.Sum(x => value(x) * weight(x)) / totalWeight).GetZeroWhenInvalid();
+        }
+    }
+}
