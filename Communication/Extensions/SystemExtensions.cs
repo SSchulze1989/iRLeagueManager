346dc8d2146292b998510d1e706a14ae28c60515
Simon Schulze <simonschulze2589@hotmail.de>
2020-11-18 00:05:31 +0100

    Implement zero value function for NaN or Infity values of double

---
+++ Communication/Extensions/SystemExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iRLeagueDatabase.Extensions
+{
+    public static class SystemExtensions
+    {
+        public static double GetZeroWhenInvalid(this double number)
+        {
+            if (double.IsInfinity(number) || double.IsNaN(number))
+            {
+                return 0;
+            }
+            return number;
+        }
+    }
+}
